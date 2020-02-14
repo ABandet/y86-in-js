44d6ddd72a49abe8034d440b68148887c5bf62df
Alexis BANDET <alexis.bandet@u-bordeaux.fr>
2020-02-14 14:26:26 +0100

    Add alu

---
+++ src/model/kernel-seq/aluEnum.ts
@@ -0,0 +1,9 @@
+enum alufct {
+    A_ADD,
+    A_AND,
+    A_SUB,
+    A_XOR,
+    A_NONE
+};
+
+export {alufct}
--- src/model/kernel-seq/sim.ts
+++ src/model/kernel-seq/sim.ts
@@ -1,15 +1,40 @@
 import {Registers, registers} from "./registers"
 import {Context} from "./context";
+import {alufct} from "./aluEnum";
 
 class Sim {
     context: Context = new Context();
     registers: Registers = new Registers();
 
     constructor() {
-        
+
     }
 
     step(){
 
+    }
+
+    alu(){
+        if (this.context.ifun > 4 || this.context.ifun < 0) {
+            throw "Invalid ifun in Sim.alu()"
+        }
+
+        if (this.context.ifun == alufct.A_ADD) {
+            this.context.valE = this.context.aluA + this.context.aluB;
+        }
+        else if (this.context.ifun == alufct.A_AND) {
+            this.context.valE = this.context.aluA & this.context.aluB;
+        }
+        else if (this.context.ifun == alufct.A_SUB) {
+            this.context.valE = this.context.aluA - this.context.aluB;
+        }
+        else if (this.context.ifun == alufct.A_XOR) {
+            this.context.valE = this.context.aluA ^ this.context.aluB;
+        }
+        else if (this.context.ifun == alufct.A_NONE) {
+            throw "A_NONE constante setted."
+        }
+
+        throw "Error, alu function (ifun) not founded."
     }
 }
