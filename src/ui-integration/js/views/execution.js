89f2b7b0f7065979ce94765afd65bcd2acccdf86
Florian <florian.simba@etu.u-bordeaux.fr>
2020-02-16 12:12:42 +0100

    fixing some css defects

--- src/ui-integration/js/views/execution.js
+++ src/ui-integration/js/views/execution.js
@@ -14,7 +14,7 @@ var ExecutionView = Backbone.View.extend({
     render: function () {
         this.$el.empty().html(this.template());
 
-        this.$('.processorstate-wrapper').append(this.processorstate.$el);
+        this.$('.processorstate').append(this.processorstate.$el);
         this.$('.object').append(this.$objcode.$el);
         this.$('.registers-wrapper').append(this.registers.$el);
         this.$('.memory').empty().append(this.memview.$el);
