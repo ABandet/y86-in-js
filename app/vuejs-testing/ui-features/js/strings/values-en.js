3a14c9e6fcb9c663fb801f6c212fe7934b1bbc49
Romain Guisset <rguisset@etu.u-bordeaux.fr>
2020-03-12 08:18:01 +0100

    Clean up

--- app/vuejs-testing/ui-features/js/strings/values-en.js
+++
@@ -1,8 +0,0 @@
-var english = {
-  appName: "Y86 Simulator",
-  headerTitle: "Y86 Simulator",
-  assembleButton: "Assemble",
-  resetButton: "Reset",
-  stepButton: "Step"
-};
-export default english;
